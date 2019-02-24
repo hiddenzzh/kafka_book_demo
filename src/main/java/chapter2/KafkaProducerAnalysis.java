f3d955b52ecad0bbb7ccac9959391a4af6e538ea
hidden <690706079@qq.com>
2019-02-24 18:37:48 +0800

    add 2-1

---
+++ src/main/java/chapter2/KafkaProducerAnalysis.java
@@ -0,0 +1,73 @@
+package chapter2;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 代码清单2-1
+ * Created by hidden.zhu on 2018/8/29.
+ */
+public class KafkaProducerAnalysis {
+    public static final String brokerList = "localhost:9092";
+    public static final String topic = "topic-demo";
+
+    public static Properties initConfig() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", brokerList);
+        props.put("key.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("client.id", "producer.client.id.demo");
+        return props;
+    }
+
+    public static Properties initNewConfig() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerList);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "producer.client.id.demo");
+        return props;
+    }
+
+    public static Properties initPerferConfig() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerList);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class.getName());
+        return props;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Properties props = initConfig();
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+//        KafkaProducer<String, String> producer = new KafkaProducer<>(props,
+//                new StringSerializer(), new StringSerializer());
+
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, "hello, Kafka!");
+        try {
+            producer.send(record);
+//            producer.send(record, new Callback() {
+//                @Override
+//                public void onCompletion(RecordMetadata metadata, Exception exception) {
+//                    if (exception == null) {
+//                        System.out.println(metadata.partition() + ":" + metadata.offset());
+//                    }
+//                }
+//            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+//        TimeUnit.SECONDS.sleep(5);
+    }
+}
