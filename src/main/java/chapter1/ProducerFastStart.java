de858f004f419c1e29c5cbd84961cb2244739a88
hidden <690706079@qq.com>
2019-02-24 18:14:21 +0800

    add first code file

---
+++ src/main/java/chapter1/ProducerFastStart.java
@@ -0,0 +1,36 @@
+package chapter1;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+/**
+ * Created by hidden.zhu on 2018/7/21.
+ */
+public class ProducerFastStart {
+    public static final String brokerList = "localhost:9092";
+    public static final String topic = "topic-demo";
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put("key.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("bootstrap.servers", brokerList);
+
+
+        KafkaProducer<String, String> producer =
+                new KafkaProducer<>(properties);
+        ProducerRecord<String, String> record =
+                new ProducerRecord<>(topic, "hello, Kafka!");
+        try {
+            producer.send(record);
+//            producer.send(record).get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        producer.close();
+    }
+}
